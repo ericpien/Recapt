f013a76c4d70d930f03836f1af82dbc67cee4946
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2023-03-25 00:21:33 -0700

    hello world flask app template

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # backend
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/response', methods=['GET'])
+def get_gpt_response():
+    if(request.method == 'GET'):
+        data = {"data": "Hello World"}
+        return jsonify(data)
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=8080)
