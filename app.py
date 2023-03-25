c3075564ea468bf793fa24b5b00d391de4b3800f
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2023-03-25 14:55:26 -0700

    updated prompts

--- app.py
+++ app.py
@@ -21,34 +21,41 @@ def get_gpt_response():
         captions = ExternalAPIs.getCaptions(url)
         if captions == "Sorry, this video's captions are not available to us.":
             return jsonify(message=captions)
+
+        if len(captions.split(' ')) <= 5000:
+            sum1 = 'Here is a summary you previously gave me. Rewrite this in better English for a video description, immediately act as if you were writing a video description as the content creator. Max word count is 400 words.'
+            sum2 = 'Summarize the following captions from a Youtube video as if you were the content creator: '
+        else:
+            sum1 = 'Summarize: '
+            sum2 = 'Summarize: '
         # get the response
-        prompt, captions = get_prompt(search_text, captions)
+        prompt, captions = get_prompt(search_text, captions, sum2)
         responses = []
         responses.append(ExternalAPIs.getGPT(prompt))
         while prompt != '':
-            prompt, captions = get_prompt(search_text, captions)
+            prompt, captions = get_prompt(search_text, captions, sum2)
             responses.append(' ' + ExternalAPIs.getGPT(prompt))
 
         if search_text == 'summarize':
             response = ' '.join(responses)
             prompt, captions = get_prompt(
-                'Here is a summary you previously gave me. Rewrite this in better English for a video description, immediately act as if you were writing a video description as the content creator. Max word count is 400 words.', response)
+                sum1, response, sum2)
             response = ExternalAPIs.getGPT(prompt)
         else:
             response = max(responses, key=len)
         return jsonify(message=response)
 
 
-def get_prompt(search_text, captions):
+def get_prompt(search_text, captions, sum2):
     if captions != '':
         captions_clip = captions[0:9500]
         if search_text == 'summarize':
-            question = 'Summarize the following captions from a Youtube video as if you were the content creator: '
+            question = sum2
             context = captions_clip
         else:
             question = 'Here is a question that I want you to answer: '
             question += search_text + '. '
-            context = 'Here are the captions for you to use to answer the question, answer in a concise manner and immediately act as if you were answering a question about your own video ' + captions_clip
+            context = 'Here are the captions for you to use to answer the question, answer immediately and as if you were answering this question about your own video ' + captions_clip
 
         prompt = question + context
 
